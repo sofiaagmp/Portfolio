686259020ce2b3706448eb62c57d4f918c509856
sofiaagmp <sofiaagmp@gmail.com>
2024-04-16 16:57:43 +0100

    Portefolio_CV
    First edition of my portefolio.

---
+++ code.js
@@ -0,0 +1,19 @@
+var aboutMe = document.getElementById('about-me');
+
+aboutMe.addEventListener('mouseover', function () {
+    aboutMe.style.backgroundColor = '#FFDB58';
+});
+
+aboutMe.addEventListener('mouseout', function () {
+    aboutMe.style.backgroundColor = '#ccc';
+})
+
+var skills = document.getElementById('skills');
+
+skills.addEventListener('mouseover', function () {
+    skills.style.backgroundColor = '#FFDB58';
+});
+
+skills.addEventListener('mouseout', function () {
+    skills.style.backgroundColor = '#ccc';
+})
